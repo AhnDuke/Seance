eca5bd50b6093c10af85fac9435ae701164a02d8
Duke Ahn <108208573+AhnDuke@users.noreply.github.com>
2023-09-20 20:10:19 -0700

    create initial file structure and configure webpack & tsconfig

---
+++ dist/webpack.config.js
@@ -0,0 +1,54 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
+import path from 'path';
+import { fileURLToPath } from 'url';
+var __filename = fileURLToPath(import.meta.url);
+var __dirname = path.dirname(__filename);
+export default {
+    entry: './src/index.tsx',
+    output: {
+        path: path.join(__dirname, '/build/dist'),
+        filename: 'bundle.js',
+    },
+    devtool: 'inline-source-map',
+    server: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+            publicPath: '/',
+        },
+        port: 6060,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3030/',
+                pathRewrite: { '^/api': '' },
+            },
+        },
+        open: true,
+        hot: true,
+        historyApiFallback: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/template.html'),
+        }),
+        new NodePolyfillPlugin(),
+    ],
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.[tj]sx?$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.s?[ca]ss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+};
